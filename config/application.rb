4a9a6014f597c2191949e208d7c628a33a81a144
Rem Zolotykh <remchi@gmail.com>
2013-11-28 22:12:05 +0200

    05 Episode: Class autoloading

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require 'simplemvc'
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 
-require 'my_pages_controller'
-
 module Blog
   class Application < Simplemvc::Application
   end
