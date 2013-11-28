397c22b397be2470f3dbdf55a0422d623c6dd0db
Rem Zolotykh <remchi@gmail.com>
2013-11-28 21:35:19 +0200

    03 Episode: Simple Routing

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController
+  def about
+    "About me"
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,9 @@
 require 'simplemvc'
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+require 'pages_controller'
+
 module Blog
   class Application < Simplemvc::Application
   end
